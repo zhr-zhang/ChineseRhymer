44fc63181b0c393e3159892038c7b4968be50add
Haoran Zhang <zhr.zhang@outlook.com>
2023-11-23 15:44:04 +0800

    Update character.py

--- character.py
+++ character.py
@@ -10,8 +10,8 @@ class Character:
         self.pinyins = [PinYin(pinyin) for pinyin in self.pinyins]
 
 
-c = Character("8BAD xùn 训")
-print(c.pinyins[0].pinyin)
-print(c.pinyins[0].vowel)
-print(c.pinyins[0].consonant)
-print(c.pinyins[0].tone)
+# c = Character("8BAD xùn 训")
+# print(c.pinyins[0].pinyin)
+# print(c.pinyins[0].vowel)
+# print(c.pinyins[0].consonant)
+# print(c.pinyins[0].tone)
